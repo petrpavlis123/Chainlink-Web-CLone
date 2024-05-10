1a5cf640b9eb4c10ed1c66cc29550b024fe7f569
Petr Pavliš <165018210+petrpavlis123@users.noreply.github.com>
2024-05-10 18:17:39 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById("swapButton").addEventListener("click", function() {
+    // Add functionality for SWAp button
+    alert("SWAp button clicked");
+});
+
+document.getElementById("aaaButton").addEventListener("click", function() {
+    // Add functionality for AAA button
+    alert("AAA button clicked");
+});
+
+document.getElementById("bbbButton").addEventListener("click", function() {
+    // Add functionality for BBB button
+    alert("BBB button clicked");
+});
